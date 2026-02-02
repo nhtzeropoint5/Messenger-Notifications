d2b987dfceed847a1a87f0bd0621406d499ef0d1
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-02-02 21:05:10 +0600

    phase 2

---
+++ script.js
@@ -0,0 +1,7 @@
+var notify = document.querySelector('.notifications');
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', active);
+
+function active(){
+    var add = Number(notifications.getAttribute('data-count'));
+}
