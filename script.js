0f5366dc0ca0fce23376ebac7e3b5ef155295bcd
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-02-02 21:13:46 +0600

    phase 3

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ var btn = document.querySelector('.btn');
 btn.addEventListener('click', active);
 
 function active(){
-    var add = Number(notifications.getAttribute('data-count'));
+    var add = Number(notify.getAttribute('data-count') || 0);
+    notify.setAttribute('data-count', add+1);
+    if(add === 0){
+        notify.classList.add('add-numb');
+    }
+    false;
 }
